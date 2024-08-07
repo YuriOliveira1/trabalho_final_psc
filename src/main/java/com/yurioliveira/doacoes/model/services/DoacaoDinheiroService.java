e475a0e11c9fa3ef817f01c819e70202958d9af9
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2024-08-06 23:41:18 -0300

    Create DoacaoDinheiroService.java

---
+++ src/main/java/com/yurioliveira/doacoes/model/services/DoacaoDinheiroService.java
@@ -0,0 +1,33 @@
+package com.yurioliveira.doacoes.model.services;
+
+import com.yurioliveira.doacoes.model.dao.DAO;
+import com.yurioliveira.doacoes.model.dao.impl.DaoFactory;
+import com.yurioliveira.doacoes.model.entities.DoacaoDinheiro;
+
+import java.util.List;
+
+public class DoacaoDinheiroService {
+
+    private DAO<DoacaoDinheiro> dao = DaoFactory.createDinheiroDao();
+
+    public List<DoacaoDinheiro> listAll() {
+        return dao.findAll();
+    }
+
+    public DoacaoDinheiro findById(int id) {
+        return dao.findById(id);
+    }
+
+    public void SaveOrUpdate(DoacaoDinheiro dinheiro) {
+        if (dinheiro.getId() == null) {
+            dao.insert(dinheiro);
+        } else {
+            dao.update(dinheiro);
+        }
+    }
+
+    public void delete(DoacaoDinheiro dinheiro) {
+        dao.deleteById(dinheiro.getId());
+    }
+    
+}
